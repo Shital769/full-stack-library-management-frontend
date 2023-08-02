79a90ee579a6dbf573d23eeffa7f2abc88a2e20a
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-08-02 19:30:35 +1000

    Build Dashboard Layout.

---
+++ src/components/Layout/DashboardLayout.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Sidebar from "../sidebar/Sidebar";
+
+const DashboardLayout = ({ children }) => {
+  //fetching user from session storage using useEffect Hooks
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const userPerson = JSON.parse(sessionStorage.getItem("user"));
+    if (userPerson) {
+      setUser(userPerson);
+    }
+  }, []);
+  return (
+    <div className="dashboard-layout">
+      <Sidebar currentUser={user} />
+      <div className="dashboard-main">
+        <Header currentUser={user} />
+        <div className="content">{children}</div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="p-5 text-center bg-dark text-lightF" >&copy; All Rights Resereved 2023 || Made with Love ❤️ </div>;
+};
+
+export default Footer;
