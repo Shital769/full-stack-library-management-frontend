7765c29cd9f07dc5c21445e5184d945078c6e90d
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-08-06 12:59:01 +1000

    build a book page.

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../components/Layout/DashboardLayout";
 import { Container, Row } from "react-bootstrap";
+import { getBooks } from "../helpers/AxiosHelpers";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  const fetchAllBooks = async () => {
+    const response = await getBooks();
+
+    setBooks(response.books);
+  };
+
+  useEffect(() => {
+    fetchAllBooks();
+  }, []);
   return (
     <DashboardLayout>
       <Container>
         <Row className="p-5">
-          <BookList />
+          <BookList books={books} fetchBooks={fetchAllBooks} />
         </Row>
       </Container>
     </DashboardLayout>
